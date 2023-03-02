dbeffd0f08c99366631f32afb3e56e037abb49d0
Michael Oddis <856914+michaeloddis@users.noreply.github.com>
2023-03-02 13:23:51 -0500

    Fixed size of checkbox

--- src/app/components/checkbox.tsx
+++ src/app/components/checkbox.tsx
@@ -22,12 +22,14 @@ export const Checkbox = ({
     
     /**
      * <div className='test block h-4 w-4 border-[#27272A] border-2 border-solid rounded checked:bg-white before:content-[""] after:w-[6px] after:h-[12px] bordered after:border-white after:border-solid after:border-t-0 after:border-r-4 after:border-b-4 after:border-l-0 after:rotate-45'></div>
+     * 
+     * text-white bg-[#27272A] border-[#3F3F46] rounded-[4px] checked:bg-[#4F46E5] checked:border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
      */
     return (
         <input
             type="checkbox"
             ref={ref}
-            className={className + ' cursor-pointer'}
+            className={className + ' cursor-pointer form-checkbox h-4 w-4'}
             {...rest}
         />
     );
