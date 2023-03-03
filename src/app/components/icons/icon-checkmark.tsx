212aa3c88c650b0e1161dc6c0cf505cc09e65384
Michael Oddis <856914+michaeloddis@users.noreply.github.com>
2023-03-03 17:14:32 -0500

    Added new checkmark and intermediate icons

---
+++ src/app/components/icons/icon-checkmark.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const IconFilter = () => {
+    return (
+        <div className='inline-flex justify-center items-center'>
+            <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.25 5.375L4.25 8.375L8.75 1.625" stroke="#FAFAFA" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>
+        </div>
+    )
+}
