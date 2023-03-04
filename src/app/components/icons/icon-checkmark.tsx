6bcdb47b5bd2f84c462aace5b399419fcedff2b0
Michael Oddis <856914+michaeloddis@users.noreply.github.com>
2023-03-03 19:06:18 -0500

    Checked styled now

--- src/app/components/icons/icon-checkmark.tsx
+++ src/app/components/icons/icon-checkmark.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const IconFilter = () => {
+export const IconCheckmark = () => {
     return (
         <div className='inline-flex justify-center items-center'>
             <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
